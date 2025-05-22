94c74a0f73a8e9d4a53c91c27ce9fa30dfe04f3f
Lois Cole <lois@nas-10-240-37-53.york.ac.uk>
2025-05-22 12:16:56 +0100

    Completed read data function

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -11,49 +11,34 @@ import pytz
 import math
 import glob
 import datetime
+import pytest
 
-now = datetime.datetime.now()
-print(now)
 
 def read_tidal_data(filename):
-    if not os.path.exists (filename):
-        raise FileNotFoundError (f'Error: No .txt files found in the directory: {dirname}')
+    # Create error message if file not found
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f'Error: File not found: {filename}')
     else:
-        main_data = read_tidal_data(filelist[0])
-        for single_file in filelist[1:]:
-            data = read_tidal_data(single_file)
-            main_data = join_data(data, main_data)
-    data = pd.read_csv(filename, sep=r'\s+', skiprows=11)
-    print(data.index[0])
-    (names = column_names,ArithmeticError
-    parse_dates={'Datetime': ['Date', 'Time']}, 
-    index_col='Datetime',                    
-    na_values=['M', 'N', 'T'])
-    
-    print('Original DataFrame:')
-    print(df)
-    df.rename(columns={'old_'})
-    
-    
+        column_names = ['Date_Col', 'Time', 'Sea Level', 'Flag']
+        parse_dates={'Datetime': ['Date_Col', 'Time']} #Combines 'Date_Col' and 'Time-Col' into a new 'Datetime' column      
+        na_values=['M', 'N', 'T']
+        data = pd.read_csv(filename, sep=r'\s+', skiprows=11, names=column_names, na_values=na_values)
+        data['Datetime'] = pd.to_datetime(data['Date_Col'] + ' ' + data['Time'])
+        data.set_index('Datetime', inplace=True)
+        data['Sea Level'] = pd.to_numeric(data['Sea Level'], errors='coerce')
+        """data = data.drop(columns=['Date_Col', 'Flag'], errors='ignore')"""
+        return data
     def test_missing_file_raises_filenotfound():
         'Tests that the read_tidal_data rasies FileNotFoundError'
         with pytest.raises(FileNotFoundError):
             read_tidal_data('missing_file.dat')
     return data
 
-#assert 'Sea Level' in columns
-#Check for M, N and T data: should be Nan
-#Error if file not found
-  
 
 def extract_single_year_remove_mean(year, data):
-    return
+    
 
-def read_tidal_data(filename):
-        with open(filename, 'r') as f:
-            return f.readlines()
-        return 
-
+    return year_data
 
 
 def extract_section_remove_mean(start, end, data):
@@ -63,9 +48,7 @@ def extract_section_remove_mean(start, end, data):
 
 
 def join_data(data1, data2):
-    
-    return 
-
+   
 
 
 def sea_level_rise(data):
@@ -86,42 +69,51 @@ def get_longest_contiguous_data(data):
     return 
 
 
-def find_txt_files(directory):
-    return glob.glob(os.path.join(directory, '*.txt'))
+"""def find_txt_files(directory):
+    return glob.glob(os.path.join(directory, '*.txt'))"""
 
 if __name__ == '__main__':
 
+  
+    """
     parser = argparse.ArgumentParser(
-                     prog="UK Tidal analysis",
-                     description="Calculate tidal constiuents and RSL from tide gauge data",
-                     epilog="Copyright 2024, Jon Hill"
-                     )
-
-    parser.add_argument("directory",
-                    help="the directory containing txt files with data")
-    parser.add_argument('-v', '--verbose',
-                    action='store_true',
-                    default=False,
-                    help="Print progress")
-
-    args = parser.parse_args()
-    dirname = args.directory
-    verbose = args.verbose
- 
-# Defining dirname 
-dirname = os.path.dirname(__file__)
+    prog="UK Tidal analysis",
+    description="Calculate tidal constiuents and RSL from tide gauge data",
+    epilog="Copyright 2024, Jon Hill"
+    )
+       
+   parser.add_argument("directory", default='/data',
+   help="the directory containing txt files with data")
+   parser.add_argument('-v', '--verbose',
+   action='store_true',
+   default=False,
+   help="Print progress")
+   
+   args = parser.parse_args()
+   dirname = args.directory
+   verbose = args.verbose
+   """
+    dirname = 'data'
+    verbose = False
+   # Defining dirname 
+    """dirname = os.path.dirname(__file__)
+    print(dirname)
+    """
     
-# glob dir to grab all *.txt files
-filelist = glob.glob(os.path.join(dirname, '*.txt'))
-main_data = read_tidal_data(filelist[0])
-for single_file in filelist[1:]:
-    data = read_tidal_data(single_file)
-    main_data = join_data(data, main_data)
+   
+   # glob dir to grab all *.txt files
+    filelist = glob.glob(os.path.join(dirname, '**/*.txt'), recursive = True)
+   
+    if filelist:
+       main_data = read_tidal_data(filelist[0])
+       for single_file in filelist[1:]:
+           data = read_tidal_data(single_file)
+           main_data = join_data(main_data, data)
+        
+      
     
+           
 
-        
-        
-        
         
         
         
