975830a722bdbd586d5cb3efbffa89fb1a71a93f
Lois Cole <lois@nas-10-240-37-53.york.ac.uk>
2025-05-22 13:06:45 +0100

    Comments for read function

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -15,36 +15,64 @@ import pytest
 
 
 def read_tidal_data(filename):
-    # Create error message if file not found
+    """
+    Reads tidal data from multiple files in a directory, processes it, and returns a pandas DataFrame.
+   
+    Parameters
+    ----------
+    filename : str
+        The path to the text file containing the tidal data.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the specified 'filename' cannot be found or does not exist.
+
+    Returns
+    -------
+    data : pandas DataFrame
+        The DataFrame will include processed tidal data with a DatetimeIndex and a 'Sea Level' column.
+            Non- numeric entries (e.g. 'M', 'N' and 'T') will be converted to NaN.
+    """
+    # Check if file exists. If not, raise error message. 
     if not os.path.exists(filename):
         raise FileNotFoundError(f'Error: File not found: {filename}')
     else:
+        #Define expected column names
         column_names = ['Date_Col', 'Time', 'Sea Level', 'Flag']
-        parse_dates={'Datetime': ['Date_Col', 'Time']} #Combines 'Date_Col' and 'Time-Col' into a new 'Datetime' column      
+        parse_dates={'Datetime': ['Date_Col', 'Time']}
+        #Specify non-numerical values that should be converted to NaN.
         na_values=['M', 'N', 'T']
+        #Read the csv file data into a pandas DataFrame.
         data = pd.read_csv(filename, sep=r'\s+', skiprows=11, names=column_names, na_values=na_values)
+        #Combine 'Date_Col' and 'Time' into a single string,
+        #convert this into a datetime object,
+        #and then set the 'Datetime' column as the DataFrame's index.
         data['Datetime'] = pd.to_datetime(data['Date_Col'] + ' ' + data['Time'])
         data.set_index('Datetime', inplace=True)
         data['Sea Level'] = pd.to_numeric(data['Sea Level'], errors='coerce')
         """data = data.drop(columns=['Date_Col', 'Flag'], errors='ignore')"""
         return data
-    def test_missing_file_raises_filenotfound():
-        'Tests that the read_tidal_data rasies FileNotFoundError'
-        with pytest.raises(FileNotFoundError):
-            read_tidal_data('missing_file.dat')
+   
+def test_missing_file_raises_filenotfound():
+#Tests that the read_tidal_data rasies FileNotFoundError.
+    with pytest.raises(FileNotFoundError):
+        read_tidal_data('missing_file.dat')
     return data
+
+
 
 
 def extract_single_year_remove_mean(year, data):
     
-
     return 
 
 
 def extract_section_remove_mean(start, end, data):
     
+    return
 
-    return 
+
 
 
 def join_data(data1, data2):
