fcb6ded5c73023e99fb6a126caca190ca78da3b5
Lois Cole <kwl520@york.ac.uk>
2025-05-24 13:09:15 +0100

    Linting

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -1,17 +1,14 @@
+'Copyright 2025, Lois Cole'
 # import the modules you need here
 import argparse
 import datetime
 import glob
-import math
 import os
 import pytz
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
-import pytest
 import uptide
-import wget
 from scipy import stats
 
 def read_tidal_data(filename):
@@ -41,11 +38,11 @@ def read_tidal_data(filename):
         raise FileNotFoundError(f'Error: File not found: {filename}')
     # Define expected column names
     column_names = ['Cycle', 'Date', 'Time', 'Sea Level', 'Residual']
-    parse_dates={'Datetime': ['Date', 'Time']}
     # Specify non-numerical values that should be converted to NaN.
     na_values=['M', 'N', 'T']
     #Read the csv file data into a pandas DataFrame.
-    data = pd.read_csv(filename, sep=r'\s+', skiprows=11, header = None, names=column_names, na_values=na_values)
+    data = pd.read_csv(filename, sep=r'\s+', skiprows=11, header = None,
+                       names=column_names, na_values=na_values)
     # Combine 'Date_Col' and 'Time' into a single string,
     # convert this into a datetime object,
     # and then set the 'Datetime' column as the DataFrame's index.
@@ -106,13 +103,11 @@ def extract_section_remove_mean(start, end, data):
     start_dt = pd.to_datetime(start, format='%Y%m%d')
     end_dt = pd.to_datetime(end, format='%Y%m%d') + pd.Timedelta(days=1) - pd.Timedelta(hours=1)
     extracted_section = data.loc[start_dt:end_dt].copy()
-   
     if 'Sea Level' not in extracted_section.columns:
-        raise ValueError("The 'Sea Level' column is missing in the provided data.")      
+        raise ValueError("The 'Sea Level' column is missing in the provided data.")
     section_mean = extracted_section['Sea Level'].mean()
     extracted_section['Sea Level'] -= section_mean
     return extracted_section
-
 
 def join_data(data1, data2):
     """
@@ -131,11 +126,11 @@ def join_data(data1, data2):
          A new DataFrame, combining all rows from data1 and data2, 
          and sorting them chronologically through their DatetimeIndex.
      """
-    # Create a list with two DataFrames to be concatenated, 
+    # Create a list with two DataFrames to be concatenated,
     # stacking the DataFrames and ordering them chronologically.
     combined_data = [data1, data2]
     return pd.concat(combined_data).sort_index()
-    
+
 def sea_level_rise(data):
     """
     Calculates the slope and p-value of sea level rise using linear regression.
@@ -158,9 +153,9 @@ def sea_level_rise(data):
     errors='coerce'
     )
     # Drop rows where datetime conversion failed
-    df_clean = df_clean.dropna(subset=['Combined_DateTime'])   
+    df_clean = df_clean.dropna(subset=['Combined_DateTime'])
     # Convert datetime objects to Matplotlib dates (floats) for linear regression
-    x = mdates.date2num(df_clean['Combined_DateTime'].dt.to_pydatetime()) 
+    x = mdates.date2num(df_clean['Combined_DateTime'].dt.to_pydatetime())
     y = df_clean['Sea Level'].values
     slope, _, _, p_value, _ = stats.linregress(x, y)
     return slope, p_value
@@ -187,32 +182,27 @@ def tidal_analysis(data, constituents, start_datetime):
     # Drop rows where 'Sea Level' data is missing (NaN)
     data = data.dropna(subset=['Sea Level']).copy()
     #Initialises an Uptide Tides object, specifiying constituents and initial time
-    tide = uptide.Tides(['M2', 'S2'])
+    tide = uptide.Tides(constituents)
     tide.set_initial_time(start_datetime)
     seconds_since = (
         data.index.astype('int64').to_numpy()/1e9
     ) - start_datetime.timestamp()
-   # Extracts 'Sea Level] column values as a NumPy array
+    # Extracts 'Sea Level' column values as a NumPy array
     sea_level_values = data['Sea Level'].to_numpy()
     amp, pha = uptide.harmonic_analysis(
-        tide, 
+        tide,
         sea_level_values,
     seconds_since
     )
     return amp, pha
-  
+
 def get_longest_contiguous_data(data):
+ 
+    return
 
-
-    return 
-
-
-"""def find_txt_files(directory):
-    return glob.glob(os.path.join(directory, '*.txt'))"""
 
 if __name__ == '__main__':
 
-  
     """
     parser = argparse.ArgumentParser(
     prog="UK Tidal analysis",
@@ -231,16 +221,14 @@ if __name__ == '__main__':
    dirname = args.directory
    verbose = args.verbose
    """
-    dirname = 'data'
-    verbose = False
-  
-    
-   # glob dir to grab all *.txt files
+    input_directory = 'data'
+    is_verbose = False 
+
+    # glob dir to grab all *.txt files
     filelist = glob.glob(os.path.join(dirname, '**/*.txt'), recursive = True)
-   
     if filelist:
         main_data = read_tidal_data(filelist[0])
         for single_file in filelist[1:]:
             data = read_tidal_data(single_file)
             main_data = join_data(main_data, data)
-                
+              
